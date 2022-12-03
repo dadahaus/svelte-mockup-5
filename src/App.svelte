54821a701bab5287b89241e376fd2634aae079ad
asus <casadadaista@gmail.com>
2022-12-03 07:38:28 -0300

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import "./dist/style.css";
+  import Hero from "./lib/Hero.svelte";
+  import Second from "./lib/Second.svelte";
+  import Third from "./lib/Third.svelte";
+  import Fourth from "./lib/Fourth.svelte";
+  import Footer from "./lib/Footer.svelte";
+</script>
+
+<main class="wrapper">
+  <Hero class="section section__1" />
+  <Second class="section section__2" />
+  <Third class="section section__3" />
+  <Fourth class="section section__4" />
+  <Footer class="section section__3" />
+</main>
+
+<style lang="scss">
+  //   section {
+  //   height: 100vh;
+  //   display: flex;
+  //   flex-flow: column;
+  //   justify-content: center;
+  //   align-items: center;
+  //   gap: 1rem;
+  // }
+</style>
